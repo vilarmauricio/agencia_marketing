c4a2ece8e5b75ec2bbdcc2d62976fa88981ac20c
Vilar Mauricio <mauriciov.innova@gmail.com>
2023-02-02 17:56:09 -0300

    Creacion Redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import store from './store'
+import { Provider } from "react-redux";
+
+import Error404 from 'containers/errors/Error404';
+import Home from 'containers/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+
+      
+      <Router>
+        <Routes>
+          {/* Error display */}
+          <Route path="*" element={<Error404 />} />
+
+          {/* Home display */}
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+      </Router>
+
+    </Provider>
   );
 }
 
